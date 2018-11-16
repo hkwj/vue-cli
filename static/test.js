176688d53c363d3293208badbf2c8f0d81b33fbe
hkwj <hongkouwuji@sina.com>
2018-11-16 22:38:36 +0800

    vue-cli

--- static/test.js
+++ static/test.js
@@ -1,8 +1,8 @@
 let fs = require('fs');
 
-fs.readFile('vue-cli/static/dataBase/1.txt','utf8',function(err,data){
+fs.readFile('1.txt','utf8',function(err,data){
     console.log('1.txt',err, data);
-    fs.readFile(`vue-cli/static/dataBase/${data}`,'utf8',function(err,data){
+    fs.readFile(`2.txt`,'utf8',function(err,data){
         console.log('2.txt',err, data);
     })
 });
